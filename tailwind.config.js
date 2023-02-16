7ab0aa095ace29bb992a2ce6a2c6de833b11498e
galactic_ <ndg72609@gmail.com>
2023-02-15 20:12:28 -0800

    24 percent done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {},
+    screens: {
+      xxsm: "280px",
+      xm: "320px",
+      sm: "640px",
+      md: "768px",
+      md2: "800px",
+      md4: "900px",
+      lg: "1024px",
+      xl: "1280px",
+      xl2: "1536px",
+    }
   },
   plugins: [],
 }
